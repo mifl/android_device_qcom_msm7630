57ca9e123465c628bbd16fc95a21a7378c1b213e
Venkat Ram Prakash Kotni <vkotni@codeaurora.org>
2012-03-28 00:41:04 +0530

    device/qcom/msm7630_surf: Start ril daemon
    After set rild.libpath to RPC RIL stop ril-daemon
    then start ril-daemon.
    
    Change-Id: Ieadde5b8461bac8eff9a98381985881646bf031f

--- init.qcom.ril.path.sh
+++ init.qcom.ril.path.sh
@@ -1,5 +1,5 @@
 #!/system/bin/sh
-# Copyright (c) 2009-2011, Code Aurora Forum. All rights reserved.
+# Copyright (c) 2009-2012, Code Aurora Forum. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
@@ -39,4 +39,5 @@ modemid=${buildid:$offset:$length}
 if [ "$modemid" = "$isrpc" ]; then
     setprop rild.libpath "/system/lib/libril-qc-1.so"
     stop ril-daemon
+    start ril-daemon
 fi
